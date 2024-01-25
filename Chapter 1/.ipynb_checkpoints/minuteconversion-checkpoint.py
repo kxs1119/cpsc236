a0dd9ae84836bd224eac02608833856bb2b961ec
Kenny Smith <122045137+lilken621@users.noreply.github.com>
2024-01-24 19:02:31 -0500

    Initial commit

---
+++ Chapter 1/.ipynb_checkpoints/minuteconversion-checkpoint.py
@@ -0,0 +1,10 @@
+
+seconds = int(input("Enter the amount of seconds: ")) # data of seconds
+minutes = seconds // 60 # converting seconds to minutes
+hours = minutes // 60   # converting minutes to hours 
+
+print("Here is the amount of seconds inputted: " + str(seconds)) # display seconds
+print("Here is the amount of minutes inputted: " + str(minutes)) # display minutes 
+print("Here is the amount of hours inputted: " + str(hours))     # display hours
+
+
